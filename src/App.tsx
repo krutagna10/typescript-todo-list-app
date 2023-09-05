04de86b4f8632a13907305e7da9c0b81fd62d2ef
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-09-05 19:09:58 +0530

    replaced useState with useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import INITIAL_TODOS from "./data/data";
 import TodoList from "./components/TodoList/TodoList";
 import Todo from "./models/todo";
 import TodoAdd from "./components/TodoAdd/TodoAdd";
 
+type ACTION_TYPE =
+  | { type: "add-todo"; newTodo: Todo }
+  | { type: "edit-todo"; editedTodo: Todo }
+  | { type: "delete-todo"; deleteId: string };
+
+function reducer(todos: Todo[], action: ACTION_TYPE): Todo[] {
+  switch (action.type) {
+    case "add-todo": {
+      return [...todos, action.newTodo];
+    }
+    case "edit-todo": {
+      return todos.map((todo) =>
+        todo.id === action.editedTodo.id ? action.editedTodo : todo,
+      );
+    }
+    case "delete-todo": {
+      return todos.filter((todo) => todo.id !== action.deleteId);
+    }
+    default: {
+      throw new Error("Invalid Action");
+    }
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState<Todo[]>(INITIAL_TODOS);
+  const [todos, dispatch] = useReducer(reducer, INITIAL_TODOS);
 
   function handleAddTodo(title: string, isCompleted: boolean) {
     const newTodo: Todo = {
@@ -13,28 +37,26 @@ function App() {
       title: title,
       isCompleted: isCompleted,
     };
-    setTodos((prevTodos) => [...prevTodos, newTodo]);
+    dispatch({ type: "add-todo", newTodo: newTodo });
   }
 
   function handleEditTodo(editedTodo: Todo) {
-    setTodos((prevTodos) => {
-      return prevTodos.map((todo) =>
-        todo.id === editedTodo.id ? editedTodo : todo,
-      );
-    });
+    dispatch({ type: "edit-todo", editedTodo: editedTodo });
   }
 
   function handleDeleteTodo(deleteId: string) {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== deleteId);
-    });
+    dispatch({ type: "delete-todo", deleteId: deleteId });
   }
 
   return (
     <div>
       <h1>Typescript Todo List App</h1>
       <TodoAdd onAddTodo={handleAddTodo} />
-      <TodoList todos={todos} onEditTodo={handleEditTodo} onDeleteTodo={handleDeleteTodo} />
+      <TodoList
+        todos={todos}
+        onEditTodo={handleEditTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
     </div>
   );
 }
